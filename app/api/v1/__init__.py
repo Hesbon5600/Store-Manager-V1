c47892225e8e198a66beb6e0969e3eb44f98f83b
Hesbon <hesbonkiptoo5600@gmail.com>
2018-10-18 21:49:53 +0300

    [Feature #161310107] Create test for posting a sale

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,12 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
-from .views import Product
-from .views import UserRegistration, UserLogin, SingleProduct
+from .views import Product, SingleProduct, Sale
+from .views import UserRegistration, UserLogin
 v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
 api = Api(v1)
 
 api.add_resource(Product, '/products')
 api.add_resource(SingleProduct, '/products/<productID>')
+api.add_resource(Sale, '/sales')
 api.add_resource(UserRegistration, '/auth/signup')
 api.add_resource(UserLogin, '/auth/login')
