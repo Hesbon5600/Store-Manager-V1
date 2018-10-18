68b2db6e89adf9b27f8f260d1cad23e5009b27d0
Hesbon <hesbonkiptoo5600@gmail.com>
2018-10-18 21:36:29 +0300

    [Feature #161325395] Create test for getting a single product endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -7,5 +7,6 @@ v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(v1)
 
 api.add_resource(Product, '/products')
+api.add_resource(SingleProduct, '/products/<productID>')
 api.add_resource(UserRegistration, '/auth/signup')
 api.add_resource(UserLogin, '/auth/login')
