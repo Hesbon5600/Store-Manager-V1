79ac34fec03434382a9324aa1a6f83cc8d1cc644
Hesbon <hesbonkiptoo5600@gmail.com>
2018-10-26 16:06:07 +0300

    [Bug #161502130] Fix documentation link

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = create_app('development')
 
 @app.route('/')
 def index():
-    return "<p><a href='https://documenter.getpostman.com/view/4074074/RWgxvFgE'>Documentation link</a></p>"
+    return "<p><a href='https://documenter.getpostman.com/view/4074074/RzZ1qN7c'>Documentation link</a></p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
